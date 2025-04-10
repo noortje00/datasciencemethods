d1968afca32fd6414bc09112359f405c8c97cf23
noortje00 <153601022+noortje00@users.noreply.github.com>
2025-04-10 15:52:06 +0200

    Update assignment_1.R
    corrected q3

--- assignment_1/assignment_1.R
+++ assignment_1/assignment_1.R
@@ -33,3 +33,28 @@ points(d$x, ci_up, type="l", lty=2)
 points(d$x, ci_lo, type="l", lty=2)
 
 # Question 3: Kernel density estimator using Epanechnikov kernel
+h_epan <- sd(tritium) * (4 / (3 * n))^(1 / 5)  # same h by Silverman's rule
+mu2_epan <- 1 / 5
+h_epan_adjusted <- h_epan * sqrt(mu2_epan)
+h_epan_adjusted
+
+d_epan <- density(tritium, bw = h_epan_adjusted, kernel = "epanechnikov")
+d_epan
+
+K22_epan <- 3 / 5 
+
+ci_up_epan <- d_epan$y + qnorm(0.975) * sqrt(d_epan$y * K22_epan) / sqrt(n * h_epan)
+ci_lo_epan <- d_epan$y - qnorm(0.975) * sqrt(d_epan$y * K22_epan) / sqrt(n * h_epan)
+
+plot(d_epan$x, d_epan$y, type = "l", col = "red", ylim = c(0, max(ci_up_epan)))
+points(tritium, rep(0, n), pch = 3)
+points(d_epan$x, ci_up_epan, type = "l", lty = 2)
+points(d_epan$x, ci_lo_epan, type = "l", lty = 2)
+
+# Question 4: Comparing the estimators with Epanechnikov and Gaussian kernels
+
+plot(d$x, d$y, type = "l", xlab = "x", ylab = "density", ylim = c(0, max(ci_up)))
+lines(d_epan$x, d_epan$y, type = "l", col = "red", ylim = c(0, max(ci_up_epan)))
+
+points(tritium, rep(0, n), pch = 3)
+legend("topright", legend = c("Gaussian", "Epanechnikov"), col = c("black", "red"), lty = c(1, 1))
